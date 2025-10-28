5e8c086336e9e2746fe0148efd9760d40ff3ad1e
mario <marionayadeluis@gmail.com>
2025-10-28 05:02:22 +0100

    "Creación de proyecto"

---
+++ src/test/java/com/compapptition/backend/BackendCompapptitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compapptition.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCompapptitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
